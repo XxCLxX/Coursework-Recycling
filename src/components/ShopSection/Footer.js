050283cf6b0da39dbb8bac9532a398e86c6add4b
Chikondi Luka <65278289+XxCLxX@users.noreply.github.com>
2022-06-23 19:57:07 +0700

    Created E-commerce Homepage
    Every aspect of the page contributes to establishing a good first impression and promoting the required action.

---
+++ src/components/ShopSection/Footer.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import styled from 'styled-components';
+import PhoneIcon from '@mui/icons-material/Phone';
+import { FaYoutube, FaTwitter, FaVk, FaTelegramPlane } from 'react-icons/fa'
+import MeetingRoom from '@mui/icons-material/MeetingRoom';
+import Mail from '@mui/icons-material/Mail';
+
+const Container = styled.div`
+display: flex;
+`
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+padding: 20px;
+`
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+margin: 20px 0px;
+`
+
+const SocialContainer = styled.div`
+display: flex;
+`
+
+const SocialIcon = styled.div`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+color: white;
+background-color: #${(props) => props.color};
+display: flex;
+align-items: center;
+justify-content: center;
+margin-right: 20px;
+`
+
+const Center = styled.div`
+flex: 1;
+padding: 20px;
+`
+
+const Title = styled.h3`
+margin-bottom: 30px;
+`
+
+const List = styled.ul`
+margin: 0;
+padding: 0;
+list-style: none;
+display: flex;
+flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+width: 50%;
+margin-bottom: 10px;
+`
+
+const Right = styled.div`
+flex: 1;
+padding: 20px;
+`
+
+const ContactItem = styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`
+
+const Payment = styled.img`
+width: 50%;
+`
+
+const Footer = () => {
+    return (
+        <Container>
+
+            <Left>
+                <Logo>Recycle</Logo>
+                <Desc>
+                    Lorem ipsum dolor sit amet. In asperiores galisum aut delectus
+                    omnis ad placeat sint qui dignissimos omnis sed placeat deserunt
+                    sed nesciunt necessitatibus vel beatae eius! Qui ipsum reprehenderit
+                    et ipsa.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <FaVk />
+                    </SocialIcon>
+                    <SocialIcon color="24A1DE">
+                        <FaTelegramPlane />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <FaTwitter />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <FaYoutube />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Women's Fashion</ListItem>
+                    <ListItem>Men's Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>Appliances</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <MeetingRoom style={{ marginRight: "10px" }} /> Lenin Ave, 36, Tomsk, Tomsk Oblast, 634050
+                </ContactItem>
+                <ContactItem>
+                    <PhoneIcon style={{ marginRight: "10px" }} /> +1 234 56 78
+                </ContactItem>
+                <ContactItem>
+                    <Mail style={{ marginRight: "10px" }} /> contact@recycle
+                </ContactItem>
+                <Payment src="https://shikstore.ru/images/logo_pay.png" />
+            </Right>
+
+        </Container>
+    )
+}
+
+export default Footer
---
+++ src/components/ShopSection/OurMission.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 60vh;
+background-color: #e1f5fe;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+`
+
+const Title = styled.h1`
+font-size: 70px;
+margin-bottom: 20px;
+color: #000;
+`
+
+const TextWrapper = styled.div`
+justify-content: center;
+align-items: center;
+padding: 0px 50px 0px 50px;
+`
+
+const Desc = styled.p`
+font-size: 24px;
+font-weight: 300;
+margin-bottom: 0px;
+color: #000;
+text-align: center;
+`
+
+const OurMission = () => {
+    return (
+        <Container>
+            <Title>Our Mission</Title>
+            <TextWrapper>
+                <Desc>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi </Desc>
+            </TextWrapper>
+        </Container>
+    )
+}
+
+export default OurMission
