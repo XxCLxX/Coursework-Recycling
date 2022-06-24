3b93f91fcd0c17bf8770092d4d382c167d40bed2
Chikondi Luka <65278289+XxCLxX@users.noreply.github.com>
2022-06-24 15:17:02 +0700

    added product-view page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages';
 import DonationPage from './pages/donation';
 import DonationForm from './pages/donation-form';
 import ShopPage from './pages/shop';
 import ProductList from './pages/ProductList';
+import ProductView from './pages/ProductView';
 
 function App() {
 
@@ -14,9 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/donation" element={<DonationPage />} />
-        <Route path="/donation-form" element={<DonationForm/>} />
-        <Route path="/shop" element={<ShopPage/>} />
-        <Route path="/products" element={<ProductList/>}/>
+        <Route path="/donation-form" element={<DonationForm />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/product-view" element={<ProductView/>} />
       </Routes>
     </Router>
   );
